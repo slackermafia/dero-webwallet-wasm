d6873c92de48c3807499c14f00206e809e491496
The OG Slacker <81060941+slackermafia@users.noreply.github.com>
2024-07-06 03:08:36 -0400

    Update go_build.sh

--- go_build.sh
+++ go_build.sh
@@ -1,6 +1,3 @@
-# copy wasm_exec.js helper to nft web app public folder
-# cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" ../dero-nft-trade/public/
-# wget https://github.com/tinygo-org/tinygo/blob/v0.26.0/targets/wasm_exec.js
 # build wasm module containing go functions
 GOOS=js GOARCH=wasm go build -o dero_wallet.wasm main.go
 cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" .
