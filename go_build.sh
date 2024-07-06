b43287b7621d1aaaa663b8b9e94812006a7d4d81
The OG Slacker <81060941+slackermafia@users.noreply.github.com>
2024-07-06 03:08:09 -0400

    Update go_build.sh

--- go_build.sh
+++ go_build.sh
@@ -2,7 +2,6 @@
 # cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" ../dero-nft-trade/public/
 # wget https://github.com/tinygo-org/tinygo/blob/v0.26.0/targets/wasm_exec.js
 # build wasm module containing go functions
-GOOS=js GOARCH=wasm go build -o ./build/dero_wallet.wasm #../dero-nft-trade/public/dero_wallet.wasm
-# tinygo build -o main.wasm -target wasm ./main.go
-cp ./build/dero_wallet.wasm ../dero-nft-trade/public
+GOOS=js GOARCH=wasm go build -o dero_wallet.wasm main.go
+cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" .
 sha512sum ./build/dero_wallet.wasm > ./build/sha512sums.txt
